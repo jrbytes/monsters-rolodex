7ea55f73ba6225403355e7969142c0d1538110cd
Itamar Junior <itamazor@gmail.com>
2020-03-06 22:19:24 -0300

    Updates

---
+++ precache-manifest.643d89e025eb4e62b8fe68f920064b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ecec36cb4f691cbff80045541e3f95",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5ddd241dabff8fa5c309",
+    "url": "/monsters-rolodex/static/css/main.90c01ecf.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ddd241dabff8fa5c309",
+    "url": "/monsters-rolodex/static/js/main.44d1d7ce.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
